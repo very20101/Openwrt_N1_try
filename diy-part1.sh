8532e9e6d1101c6ad9af8da38366cbc09ca4aa22
welvery2010 <very20101@users.noreply.github.com>
2022-05-01 00:45:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,6 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo  'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo  'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-echo  'src-git routing https://github.com/coolsnowwolf/routing' >>feeds.conf.default
 
 
 
