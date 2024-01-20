927b069205c5f21182fb2028938852b968a6baaf
welvery2010 <very20101@users.noreply.github.com>
2024-01-20 09:08:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,7 +64,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 rm -rf feeds/packages/lang/rust
 merge_package https://github.com/openwrt/packages packages/lang/rust
 rm -rf package/lean/libcryptopp
-merge_package https://github.com/very20101/Openwrt_N1_try/libcryptopp packages/libcryptopp
+merge_package https://github.com/very20101/Openwrt_N1_try Openwrt_N1_try/libcryptopp
 rm -rf package/feeds/packages/ruby
 merge_package https://github.com/openwrt/packages  packages/lang/ruby
 rm -rf feeds/packages/net/unbound
