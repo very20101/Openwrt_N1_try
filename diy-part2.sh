469216dd3c6881f92ca48c6806dbd70d58ae0ded
welvery2010 <very20101@users.noreply.github.com>
2025-12-30 08:21:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -93,7 +93,7 @@ cp -rf package/openwrt_N1_test/extra_pack/openwrt-tools/erofs-utils tools/erofs-
 rm -rf package/libs/libnl-tiny package/system/fwtool package/libs/libjson-c package/libs/libubox  
 rm -rf package/system/ubus package/system/uci feeds/packages/net/softethervpn5 package/system/ucert 
 rm -rf package/network/config/netifd feeds/packages/libs/lzo package/libs/udebug feeds/packages/utils/tini
-rm -rf feeds/packages/libs/expat package/system/ubox package/libs/libcap package/libs/zlib
+rm -rf feeds/packages/libs/expat package/system/ubox package/libs/libpcap package/libs/zlib package/system/fstools
 cp -rf package/openwrt_N1_test/extra_pack/libnl-tiny package/libs/libnl-tiny
 cp -rf package/openwrt_N1_test/extra_pack/fwtool package/system/fwtool
 cp -rf package/openwrt_N1_test/extra_pack/libjson-c package/libs/libjson-c
@@ -108,8 +108,9 @@ cp -rf package/openwrt_N1_test/extra_pack/lzo feeds/packages/libs/lzo
 cp -rf package/openwrt_N1_test/extra_pack/tini feeds/packages/utils/tini
 cp -rf package/openwrt_N1_test/extra_pack/expat feeds/packages/libs/expat
 cp -rf package/openwrt_N1_test/extra_pack/ubox package/system/ubox
-cp -rf package/openwrt_N1_test/extra_pack/libcap package/libs/libcap
+cp -rf package/openwrt_N1_test/extra_pack/libpcap package/libs/libpcap
 cp -rf package/openwrt_N1_test/extra_pack/zlib package/libs/zlib
+cp -rf package/openwrt_N1_test/extra_pack/fstools package/system/fstools
 
 rm -rf package/openwrt_N1_test
 
