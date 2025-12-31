006a3c312e2d4c2cf5933ffcb74ecba752618b6f
welvery2010 <very20101@users.noreply.github.com>
2025-12-31 15:33:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -84,9 +84,10 @@ cp -rf package/openwrt_N1_test/extra_pack/miniupnpd feeds/packages/net/miniupnpd
 #cp -rf package/openwrt_N1_test/extra_pack/openwrt-keyring package/system/openwrt-keyring
 #rm -rf package/base-files/Makefile
 #cp -rf package/openwrt_N1_test/extra_pack/base-files_Makefile package/base-files/Makefile
+rm -rf tools/erofs-utils
+cp -rf package/openwrt_N1_test/extra_pack/erofs-utils tools/erofs-utils
 
-
-rm -rf package/openwrt_N1_test
+#rm -rf package/openwrt_N1_test
 
 rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
 
