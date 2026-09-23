910451a086f6a69b1eefb48dc72a40928d7a95f4
welvery2010 <very20101@users.noreply.github.com>
2026-09-23 10:13:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -74,10 +74,17 @@ rm -rf feeds/packages/net/adblock feeds/luci/applications/luci-app-adblock
 cp -rf package/openwrt_N1_test/extra_pack/adblock feeds/packages/net/adblock
 rm -rf feeds/luci/applications/luci-app-adblock
 cp -rf package/openwrt_N1_test/extra_pack/luci-app-adblock feeds/luci/applications/luci-app-adblock
+cp -rf package/openwrt_N1_test/extra_pack/luci-lib-uqr feeds/luci/lib/luci-lib-uqr
+
+#cp -rf package/openwrt_N1_test/extra_pack/adblockN feeds/packages/net/adblock
+#cp -rf package/openwrt_N1_test/extra_pack/luci-app-adblockN feeds/luci/applications/luci-app-adblock
+
 rm -rf feeds/packages/net/miniupnpc
 cp -rf package/openwrt_N1_test/extra_pack/miniupnpc feeds/packages/net/miniupnpc
 rm -rf feeds/packages/net/miniupnpd
 cp -rf package/openwrt_N1_test/extra_pack/miniupnpd feeds/packages/net/miniupnpd
+
+
 
 ## package/system/openwrt-keyring
 #rm -rf package/system/openwrt-keyring
